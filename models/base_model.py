6feb73eba744f4db9218665351212ca5dec06e8a
Divine-kuzo <d.kuzo@alustudent.com>
2025-09-08 00:01:21 +0200

    base

---
+++ models/base_model.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+import uuid
+from datetime import datetime
+
+class BaseModel:
+    
+
+    def __init__(self, *args, **kwargs):
+    
+        if kwargs:  # case: creating from a dictionary
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    setattr(self, key, datetime.fromisoformat(value))
+                elif key != "__class__":
+                    setattr(self, key, value)
+        else:  # case: creating a brand new instance
+            self.id = str(uuid.uuid4())  # unique ID
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+
+    def __str__(self):
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+
+    def save(self):
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        obj_dict = self.__dict__.copy()
+        obj_dict["__class__"] = self.__class__.__name__
+        obj_dict["created_at"] = self.created_at.isoformat()
+        obj_dict["updated_at"] = self.updated_at.isoformat()
+        return obj_dict
+
+

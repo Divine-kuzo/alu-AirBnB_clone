8af786ccb692d1b0e59b2ba8fb29e3c6b505dbcf
Divine-kuzo <d.kuzo@alustudent.com>
2025-09-19 12:35:29 +0200

    base

--- models/base_model.py
+++ models/base_model.py
@@ -1,54 +1,31 @@
-#!/usr/bin/python3
-"""
-BaseModel class definition
-"""
+from models import storage
 import uuid
 from datetime import datetime
 
-
 class BaseModel:
-    """
-    BaseModel class that defines all common attributes/methods for other
-    classes
-    """
-
     def __init__(self, *args, **kwargs):
-        """
-        Initialize BaseModel instance
-        """
-        if kwargs:  # case: creating from a dictionary
+        if kwargs:
+            # reconstruct from dictionary
             for key, value in kwargs.items():
-                if key == "created_at" or key == "updated_at":
-                    setattr(self, key, datetime.fromisoformat(value))
-                elif key != "__class__":
+                if key == 'created_at' or key == 'updated_at':
+                    value = datetime.fromisoformat(value)
+                if key != '__class__':
                     setattr(self, key, value)
-            # Add to storage for instances created from dictionary
-            from models import storage
-            storage.new(self)
-        else:  # case: creating a brand new instance
-            self.id = str(uuid.uuid4())  # unique ID
+        else:
+            self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            # Add to storage for new instances
-            from models import storage
-            storage.new(self)
-
-    def __str__(self):
-        """String representation of the instance"""
-        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+            storage.new(self)   # ✅ Add to storage
 
     def save(self):
-        """Updates the public instance attribute updated_at with current
-        datetime"""
+        """Updates updated_at and saves to storage"""
         self.updated_at = datetime.now()
-        from models import storage
         storage.save()
 
     def to_dict(self):
-        """Returns a dictionary containing all keys/values of __dict__ of the
-        instance"""
-        obj_dict = self.__dict__.copy()
-        obj_dict["__class__"] = self.__class__.__name__
-        obj_dict["created_at"] = self.created_at.isoformat()
-        obj_dict["updated_at"] = self.updated_at.isoformat()
-        return obj_dict
+        """Returns dictionary for JSON serialization"""
+        d = self.__dict__.copy()
+        d['__class__'] = self.__class__.__name__
+        d['created_at'] = self.created_at.isoformat()
+        d['updated_at'] = self.updated_at.isoformat()
+        return d

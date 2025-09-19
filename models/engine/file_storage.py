cf05130e97717af8380295defb74c5ec273b6aab
Divine-kuzo <d.kuzo@alustudent.com>
2025-09-19 13:16:39 +0200

    storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,36 +1,34 @@
 #!/usr/bin/python3
+"""FileStorage class to serialize/deserialize objects to JSON"""
 import json
-import os
-from models.base_model import BaseModel  # import any other models you add later
+from os.path import exists
 
 class FileStorage:
     """Serializes instances to a JSON file & deserializes back"""
-
     __file_path = "file.json"
     __objects = {}
 
     def all(self):
-        """Return the dictionary of objects"""
-        return FileStorage.__objects
+        """Return the dictionary of stored objects"""
+        return self.__objects
 
     def new(self, obj):
-        """Store object in __objects using <class name>.id as key"""
+        """Add new object with key <class name>.id"""
         key = f"{obj.__class__.__name__}.{obj.id}"
-        FileStorage.__objects[key] = obj
+        self.__objects[key] = obj
 
     def save(self):
         """Serialize __objects to JSON file"""
-        with open(FileStorage.__file_path, "w", encoding="utf-8") as f:
-            json.dump(
-                {k: v.to_dict() for k, v in FileStorage.__objects.items()}, f
-            )
+        with open(self.__file_path, "w") as f:
+            json.dump({k: v.to_dict() for k, v in self.__objects.items()}, f)
 
     def reload(self):
-        """Deserialize JSON file to __objects (if it exists)"""
-        if os.path.exists(FileStorage.__file_path):
-            with open(FileStorage.__file_path, "r", encoding="utf-8") as f:
-                data = json.load(f)
-                for key, value in data.items():
-                    cls_name = value["__class__"]
-                    if cls_name == "BaseModel":
-                        FileStorage.__objects[key] = BaseModel(**value)
+        """Deserialize JSON file to __objects if file exists"""
+        if exists(self.__file_path):
+            from models.base_model import BaseModel
+            classes = {"BaseModel": BaseModel}
+            with open(self.__file_path, "r") as f:
+                objs = json.load(f)
+                for k, v in objs.items():
+                    cls = classes[v["__class__"]]
+                    self.__objects[k] = cls(**v)
